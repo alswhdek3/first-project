a1a3de3dee70ac47fbfbc47df02c0ad51a363410
MinJong <alswhdek3>
2022-03-16 11:04:51 +0900

    스크립트 추가
    싱글톤 스크립트 추가

---
+++ Assets/Scripts/Global/SingtonMonoBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingtonMonoBehaviour<T> : MonoBehaviour where T : SingtonMonoBehaviour<T>
+{
+    static public T Instance { get; private set; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = (T)this;
+            OnAwake();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        if(Instance == (T)this)
+        {
+            OnStart();
+        }
+    }
+    virtual protected void OnAwake()
+    {
+
+    }
+    virtual protected void OnStart()
+    {
+
+    }
+}
